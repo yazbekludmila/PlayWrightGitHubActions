44f4d7bf72b920fb9319f84e548a1ad8853b7b84
yazbekludmila <yazbekludmila@gmail.com>
2025-08-19 08:20:02 -0300

    sucesso comiit 19 agosto lud

--- node_modules/playwright-core/bin/reinstall_chrome_beta_linux.sh
+++ node_modules/playwright-core/bin/reinstall_chrome_beta_linux.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 set -x
 
@@ -7,15 +7,17 @@ if [[ $(arch) == "aarch64" ]]; then
   exit 1
 fi
 
-if [[ ! -f "/etc/os-release" ]]; then
-  echo "ERROR: cannot install on unknown linux distribution (/etc/os-release is missing)"
-  exit 1
-fi
+if [ -z "$PLAYWRIGHT_HOST_PLATFORM_OVERRIDE" ]; then
+  if [[ ! -f "/etc/os-release" ]]; then
+    echo "ERROR: cannot install on unknown linux distribution (/etc/os-release is missing)"
+    exit 1
+  fi
 
-ID=$(bash -c 'source /etc/os-release && echo $ID')
-if [[ "${ID}" != "ubuntu" ]]; then
-  echo "ERROR: cannot install on $ID distribution - only Ubuntu is supported"
-  exit 1
+  ID=$(bash -c 'source /etc/os-release && echo $ID')
+  if [[ "${ID}" != "ubuntu" && "${ID}" != "debian" ]]; then
+    echo "ERROR: cannot install on $ID distribution - only Ubuntu and Debian are supported"
+    exit 1
+  fi
 fi
 
 # 1. make sure to remove old beta if any.
--- tests/teste.spec.js
+++ tests/teste.spec.js
@@ -1,22 +1,19 @@
-const { chromium } = require('playwright');
-const { test } = require('@playwright/test');
+// tests/teste.spec.js
+const { test, expect } = require('@playwright/test');
 
-test('basic test', async () => {
-    const browser = await chromium.launch({headless:true})
-    const context = await browser.newContext();
-    const page = await context.newPage()
+test('basic test', async ({ page }) => {
+  await page.goto('https://react-redux.realworld.io/#/login');
 
-    await page.goto('https://react-redux.realworld.io/#/login')
+  await page.fill('input[type="email"]', 'alanvoigt@yahoo.com.br');
+  await page.press('input[type="email"]', 'Tab');
+  await page.type('input[type="password"]', 'test123');
+  await page.click('form >> text=Sign in');
 
-    await page.fill('input[type = "email"]', 'alanvoigt@yahoo.com.br')
-    await page.press('input[type = "email"]', 'Tab')
-    await page.type('input[type = "password"]', 'test123')
-    await page.click("form >> 'Sign in'")
+  //const html = await page.innerHTML('.feed-toggle');
 
-    const html = await page.innerHTML('.feed-toggle')
+  // Screenshot
+  await page.screenshot({ path: 'SignIn.png', fullPage: true });
 
-    // Screenshot
-    await page.screenshot({path: 'SignIng.png', fullPage: true})
-
-    await browser.close()
-})
+  // Exemplo de verificação
+//  await expect(page.locator('.feed-toggle')).toContainText('Global Feed');
+});
